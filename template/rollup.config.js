46aa7b7358d74f795edecc1fe3da511040ab6eb4
Thomas Ghysels <info@thomasg.be>
2016-09-24 12:42:07 +0200

    Update dependencies

--- template/rollup.config.js
+++ template/rollup.config.js
@@ -1,15 +1,17 @@
 import vue from 'rollup-plugin-vue2'
+import css from 'rollup-plugin-css-only'
+import buble from 'rollup-plugin-buble'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-import buble from 'rollup-plugin-buble'
 import uglify from 'rollup-plugin-uglify'
 
 const plugins = [
   vue(),
+  css(),
   buble({
     exclude: 'node_modules/**'
   }),
-  nodeResolve({ browser: true, jsnext: true, main: true }),
+  nodeResolve({ browser: true, jsnext: true }),
   commonjs()
 ]
 
